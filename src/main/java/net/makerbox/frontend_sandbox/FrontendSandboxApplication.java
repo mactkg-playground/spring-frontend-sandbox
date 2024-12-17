3cd1ceb630440c6fb48b4b5fab7386092ffa666f
mactkg <mactkg@gmail.com>
2024-12-17 14:16:46 +0900

    init with spring initializr

---
+++ src/main/java/net/makerbox/frontend_sandbox/FrontendSandboxApplication.java
@@ -0,0 +1,13 @@
+package net.makerbox.frontend_sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/makerbox/frontend_sandbox/FrontendSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package net.makerbox.frontend_sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
