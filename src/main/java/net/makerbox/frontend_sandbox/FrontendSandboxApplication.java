bc024efade79dedb420112cbeb4c787012c42680
mactkg <mactkg@gmail.com>
2024-12-19 12:00:07 +0900

    Start using thymeleaf template

--- src/main/java/net/makerbox/frontend_sandbox/FrontendSandboxApplication.java
+++ src/main/java/net/makerbox/frontend_sandbox/FrontendSandboxApplication.java
@@ -2,11 +2,14 @@ package net.makerbox.frontend_sandbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class FrontendSandboxApplication {
 
 	public static void main(String[] args) {
@@ -14,7 +17,18 @@ public class FrontendSandboxApplication {
 	}
 
 	@RequestMapping("/")
+	@ResponseBody
 	String index() {
-		return "Hello World!";
+		return """
+				Hi. You can use return response using string literal when
+				<code>@ResponseBody</code> annotation is added to request handler.
+				""";
+	}
+
+
+	@RequestMapping("/hello")
+	public String hello(@RequestParam(value="name", required=true) String name, Model model) {
+		model.addAttribute("name", name);
+		return "hello";
 	}
 }
